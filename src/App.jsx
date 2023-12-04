2c5e20f6ab9b7779205537f4178de3b6369ec20d
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-04 23:09:08 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import About from "./About";
+import Missing from "./Missing";
+import PostPage from "./PostPage";
+import Nav from "./Nav";
 function App() {
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <Header />
+        <Nav />
+        <Home />
+        <NewPost />
+        <PostPage />
+        <About />
+        <Missing />
+        <Footer />
+      </div>
     </>
   );
 }
